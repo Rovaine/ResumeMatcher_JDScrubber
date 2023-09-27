ff724d6ec663d89914315c4998123f941b5d6444
Biswatosh01 <101652879+Biswatosh01@users.noreply.github.com>
2023-09-27 22:34:22 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -39,7 +39,7 @@ for i in range(doc.page_count):
       lnks.append(obj["uri"])
 # print(lnks)
 
-ll=["https://github.com/rodrigomasiniai/ResumeScreeningApp","https://github.com/Biswatosh01/Construction-Site-Safety-"]
+ll=["https://github.com/rodrigomasiniai/ResumeScreeningApp","https://github.com/Biswatosh01/Construction-Site-Safety-","https://github.com/srbhr/Resume-Matcher"]
 for lnk in ll:
     github_url = lnk + "/blob/main/README.md"
     url = convert_github_url_to_raw_url(github_url)
@@ -66,6 +66,25 @@ for lnk in ll:
         info=[]
         for p_tag in p_tags:
             info.append(p_tag.get_text())
+        clean_info = [re.sub(r'<[^>]*>', '', item).strip() for item in info if item.strip()]
+        # Define a regular expression to match emojis and symbols
+        emoji_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F"  # Emoticons
+                                u"\U0001F300-\U0001F5FF"  # Symbols & Pictographs
+                                u"\U0001F680-\U0001F6FF"  # Transport & Map Symbols
+                                u"\U0001F700-\U0001F77F"  # Alphabetic Presentation Forms
+                                u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+                                u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+                                u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+                                u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+                                u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+                                u"\U0001F004-\U0001F0CF"  # Mahjong Tiles
+                                u"\U0001F170-\U0001F251"  # Enclosed Ideographic Supplement
+                                "]+", flags=re.UNICODE)
+
+        # Remove emojis, symbols, and empty strings
+        info = [re.sub(emoji_pattern, '', item).strip() for item in clean_info if item.strip()]
+
         print(info)
     else:
         print("Failed to retrieve the README file. Status code:", response.status_code)
